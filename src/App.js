e4190fc6177b34f3a3e1a0fdb51a06793f995b15
Pro-official <promise.iam00@gmail.com>
2021-09-09 21:18:52 +0600

    api fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
-import { React } from "react";
-import { Button } from "@material-ui/core";
+import { React, useEffect, useState } from "react";
 import News from "./components/News/News";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    const url = `https://newsapi.org/v2/everything?q=Apple&from=2021-09-09&sortBy=popularity&apiKey=c69bf7b10fbf4b17a41b605900c20f54`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setArticles(data.articles));
+  }, []);
   return (
     <div>
-      <Button color="primary">Hello World</Button>;<News></News>
+      <h2>Headlines: {articles.length}</h2>
+      {articles.map((article) => (
+        <News article={article}></News>
+      ))}
     </div>
   );
 }
